4e2520f7fd18c56dafc14940d29345736e4c65de
Ekaterina Koshevaya <uea06@mail.ru>
2021-02-28 17:30:29 +0400

    Updates

---
+++ precache-manifest.be8b0933b4e6bc2c8f91dbd8ac41af6b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462ab784168932d63fe63d9c97672fd0",
+    "url": "/fotball-statistics/index.html"
+  },
+  {
+    "revision": "87e87b4d8445d97082d9",
+    "url": "/fotball-statistics/static/css/2.61833881.chunk.css"
+  },
+  {
+    "revision": "25fdbc0bcedc044b474f",
+    "url": "/fotball-statistics/static/css/main.0a33663c.chunk.css"
+  },
+  {
+    "revision": "87e87b4d8445d97082d9",
+    "url": "/fotball-statistics/static/js/2.18a924a5.chunk.js"
+  },
+  {
+    "revision": "10c32405ee75161f0d737d3fab388ca8",
+    "url": "/fotball-statistics/static/js/2.18a924a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25fdbc0bcedc044b474f",
+    "url": "/fotball-statistics/static/js/main.b2c0982d.chunk.js"
+  },
+  {
+    "revision": "f428df7ed21da21b4814",
+    "url": "/fotball-statistics/static/js/runtime-main.9a29018c.js"
+  },
+  {
+    "revision": "2d3efbfa6dd50e954fe08893b9f017fc",
+    "url": "/fotball-statistics/static/media/logo2.2d3efbfa.png"
+  },
+  {
+    "revision": "9b6cae1577b755be1231723b3dcc79ac",
+    "url": "/fotball-statistics/static/media/teams.9b6cae15.png"
+  }
+]);
